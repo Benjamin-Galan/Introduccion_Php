67ca8bdd0fc88d213cd63a479426f4030136363f
Benjamin Galan <begalan@outlook.es>
2024-07-10 14:16:55 -0600

    Sobre tipos de datos

--- 03-tipos-dato.php
+++ 03-tipos-dato.php
@@ -1,6 +1,29 @@
 <?php include 'includes/header.php';
+?>
 
+<ul>
+    <li>
+        <?php
+        //booleanos
+        $logueado = true;
+        var_dump($logueado);
+        ?>
+    </li>
+    <li>
+        <?php
+        //enteros
+        $numero = 200;
+        var_dump($numero);
+        ?>
+    </li>
+    <li>
+        <?php
+        //flotantes
+        $float = 200.5;
+        var_dump($float);
+        ?>
+    </li>
+</ul>
 
-
-
+<?php
 include 'includes/footer.php';
