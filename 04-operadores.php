fd7eaec3faaf15a0bf1c2c21177b985f66abe40c
Benjamin Galan <begalan@outlook.es>
2024-07-10 14:24:19 -0600

    Sobre operadores aritmeticos

--- 04-operadores.php
+++ 04-operadores.php
@@ -1,6 +1,29 @@
 <?php include 'includes/header.php';
 
+$numero1 = 20;
+$numero2 = 10;
 
+//suma 
+echo $numero1 + $numero2;
 
+echo "<br>";
+
+//resta
+echo $numero1 - $numero2;
+
+echo "<br>";
+
+//multiplicacion
+echo $numero1 * $numero2;
+
+echo "<br>";
+
+//division 
+echo $numero1 / $numero2;
+
+echo "<br>";
+
+//potencia
+echo  5 ** 2;
 
 include 'includes/footer.php';
