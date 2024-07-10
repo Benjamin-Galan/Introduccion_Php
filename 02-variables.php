a9bc8890ca23756bb75bfe82ca38bad2c25dc4ac
Benjamin Galan <begalan@outlook.es>
2024-07-10 14:08:12 -0600

    Aprendiendo Php

---
+++ 02-variables.php
@@ -0,0 +1,19 @@
+<?php include 'includes/header.php';
+
+$nombre = "Juan";
+
+
+$nombre = "Juan 2";
+var_dump($nombre);
+
+
+define('constante', "Valor de la constante");
+
+echo constante;
+
+const constante_2 = "Constante 2";
+echo constante_2;
+
+
+
+include 'includes/footer.php';
