20843401f5086882a9ca46987cf93632c49796d2
Benjamin Galan <begalan@outlook.es>
2024-07-11 16:52:15 -0600

    Sobre arreglos indexados

--- 08-arrays.php
+++ 08-arrays.php
@@ -1,6 +1,36 @@
 <?php include 'includes/header.php';
 
+$carrito = ['Tablet', 'Television', 'Computadora'];
 
+echo "<pre>";
+var_dump($carrito);
+echo "</pre>";
 
+//sobreescribiendo el dato de un arreglo
+$carrito[2] = 'Teclado'; 
+
+//Ingresando un elemento en un nuevo espacio del arreglo
+$carrito[3] = 'Telefono'; 
+
+echo "<pre>";
+var_dump($carrito);
+echo "</pre>";
+
+//Ingresar elementos al final de un arreglo
+array_push($carrito, 'Parlantes');
+
+//Ingresar elementos al inicio de un arreglo
+array_unshift($carrito, 'Escritorio');
+
+echo "<pre>";
+var_dump($carrito);
+echo "</pre>";
+
+//Otra forma de escribir arreglos
+$carrito2 = array('arroz', 'frijoles', 'aceite');
+
+echo "<pre>";
+var_dump($carrito2);
+echo "</pre>";
 
 include 'includes/footer.php';
