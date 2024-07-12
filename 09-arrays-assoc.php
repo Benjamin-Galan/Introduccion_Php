46ea01558f370946fc4fada4af588389f19028fd
Benjamin Galan <begalan@outlook.es>
2024-07-11 22:46:11 -0600

    Sobre arreglos asociativos u objetos

--- 09-arrays-assoc.php
+++ 09-arrays-assoc.php
@@ -1,6 +1,21 @@
 <?php include 'includes/header.php';
 
+$cliente = [
+    'nombre' => 'Juan ',
+    'saldo' => 200,
+    'informacion' => [
+        'tipo' => 'premium'
+    ]
+];
 
+echo "<pre>";
+var_dump($cliente['informacion']);
+echo "</pre>";
 
+$cliente['codigo'] = 12345545;
+
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
 
 include 'includes/footer.php';
