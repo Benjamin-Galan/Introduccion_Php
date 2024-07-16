22140d1c83cf52c7d9636385dc8366071392bc59
Benjamin Galan <begalan@outlook.es>
2024-07-15 23:34:01 -0600

    Sobre funciones de arrays

--- 11-funciones-arrays.php
+++ 11-funciones-arrays.php
@@ -1,7 +1,45 @@
 <?php include 'includes/header.php';
 
+$carrito = ['Tablet', 'Computadora', 'Television'];
 
+var_dump(in_array('Tablet', $carrito));
+var_dump(in_array('Audifonos', $carrito));
 
+//Ordenar elementos de un arreglo
+$numeros = [5, 3, 4, 2, 1, 6, 7];
+sort($numeros);
+
+echo "<pre>";
+var_dump($numeros);
+echo "</pre>";
+
+//Ordenarlos de mayor a menor
+rsort($numeros);
+
+echo "<pre>";
+var_dump($numeros);
+echo "</pre>";
+
+//ordenar arreglo asociativo
+$cliente = [
+    'saldo' => 200,
+    'tipo' => 'Premium',
+    'nombre' => 'Benjamin'
+];
+
+asort($cliente); //Ordena el arreglo por valores 
+arsort($cliente); //Ordena el arreglo por valores
+
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
+
+ksort($cliente); //Ordena el arreglo por llaves (ordenn alfabetico a - z)
+krsort($cliente); //Ordena el arreglo por llaves (orden alfabetizo z - a)
+
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
 
 
 include 'includes/footer.php';
