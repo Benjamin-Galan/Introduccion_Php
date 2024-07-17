52ffd3d72d8d5b3c743ec36fd0be18bf69b88558
Benjamin Galan <begalan@outlook.es>
2024-07-17 16:33:00 -0600

    Finalizado

--- 18.json.php
+++ 18.json.php
@@ -1,7 +1,32 @@
 <?php include 'includes/header.php';
 
+$productos = [
+    [
+        'nombre' => 'tablet',
+        'precio' => '200',
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'television',
+        'precio' => '300',
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'monitor',
+        'precio' => '400',
+        'disponible' => false
+    ],
+];
 
+echo "<pre>";
+var_dump($productos);
+echo "<pre/>";
 
+$json = json_encode($productos, JSON_UNESCAPED_UNICODE);
+$json_array = json_decode($json);
+
+var_dump($json);
+var_dump($json_array);
 
 
 
